11e8e171f0edb5a1595544f3c32093f9203a7e2e
X_Trial_Test <1trialandtest@gmail.com>
2023-03-05 21:30:29 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import Pokemon from './mycomponent/pokemon';
+
+function App() {
+  return (
+    <div className='App'>
+      <Pokemon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mycomponent/pokemon.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect } from 'react';
+import './pokemon.scss';
+let [name, setName] = useState([]);
+let Pokemon = () => {
+  return <></>;
+};
+
+export default Pokemon;
